a147a098bac37fac099b29d1651a00a12d846d1f
Omar Cuenca <A01378844@itesm.mx>
2019-09-03 23:48:48 -0500

    Failed attempt Testing still going

--- Assignment2/Challenges_Assignment2/challenge2.c
+++ Assignment2/Challenges_Assignment2/challenge2.c
@@ -12,8 +12,13 @@
 *
 */
 
-#define NUMBER_OF_POINTS 100000
-#define RANGE_OF_NUMBERS 100000
+//For longer tests
+// #define NUMBER_OF_POINTS 100000
+// #define RANGE_OF_NUMBERS 100000
+
+//For fast tests
+#define NUMBER_OF_POINTS 100
+#define RANGE_OF_NUMBERS 10000
 #define ABSURD_DISTANCE  100000
 
 //Point
@@ -24,11 +29,10 @@ typedef struct {
 
 int generateMatrix(Point *empty_Matrix,int length);
 long double distancePoints(Point A, Point B);
-long traditionalClosestNeighbor(Point *matrix,int matrixLength);
-long personalClosestNeighbor(Point *matrix,int matrixLength);
-char* printPoint(Point);
-int test();
-int findingNearestNeighbor(Point *matrix,int matrixLength,int pivotPoint,int *numberOfPointsVisited,double borderRadius,double targetRadius,char *visitedPointsArray);
+long traditionalClosestNeighbor(Point *matrix,int matrixLength,int *result);
+long personalClosestNeighbor(Point *matrix,int matrixLength,int *result);
+int test(int *answer1,int *answer2,int length);
+int findingNearestNeighbor(Point *matrix,int matrixLength,int pivotPoint,int *numberOfPointsVisited,double borderRadius,double targetRadius,char *visitedPointsArray,int *result);
 
 int main(int argc, char const *argv[]) {
 
@@ -36,17 +40,32 @@ int main(int argc, char const *argv[]) {
   Point points[NUMBER_OF_POINTS];
   generateMatrix(&points[0], NUMBER_OF_POINTS);
 
+  int resultTraditional[NUMBER_OF_POINTS];
+  int resultOptimized[NUMBER_OF_POINTS]={-1};
+
   clock_t begin = clock();
-  traditionalClosestNeighbor(&points[0],NUMBER_OF_POINTS);
+  traditionalClosestNeighbor(&points[0],NUMBER_OF_POINTS,&resultTraditional[0]);
   clock_t end = clock();
   double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
   printf("The time spent by the traditional algorithm was %f\n", time_spent);
 
   begin = clock();
-  personalClosestNeighbor(&points[0],NUMBER_OF_POINTS);
+  personalClosestNeighbor(&points[0],NUMBER_OF_POINTS,&resultOptimized[0]);
   end = clock();
   time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
   printf("The time spent by the optimized algorithm was %f\n", time_spent);
+
+  for(int i=0;i<5;i++){
+    printf("before test %d,%d\n",resultTraditional[i],resultOptimized[i]);
+  }
+
+  int boolean = test(&resultTraditional[0],&resultOptimized[0],NUMBER_OF_POINTS);
+  if(boolean){
+    printf("The result is correct\n");
+  }
+  else{
+    printf("The result is wrong\n");
+  }
 
   // printf("%lu\n", traditionalClosestNeighbor(&points[0],dimension));
   // printf("%lu\n", personalClosestNeighbor(&points[0],dimension));
@@ -76,22 +95,15 @@ long double distancePoints(Point A,Point B){
   long a,b;
   a=(A.x-B.x);
   b=(A.y-B.y);
-  // printf("%ld\n", a);
-  // printf("%ld\n", b);
   a*=a;
   b*=b;
-  // printf("%ld\n", a);
-  // printf("%ld\n", b);
-  // long double c = sqrt(a+b);
-  // printf("%LF\n", c);
-  // return c;
   return sqrt(a+b);
 }
 
 /**
 *Es un algoritmo de busqueda de vecino más cercano tradicional que tiene complejidad O(n^2).
 */
-long traditionalClosestNeighbor(Point *matrix,int matrixLength){
+long traditionalClosestNeighbor(Point *matrix,int matrixLength,int *result){
   long numberOfOperations = 0;
 
   int closestNeighbor;
@@ -110,6 +122,7 @@ long traditionalClosestNeighbor(Point *matrix,int matrixLength){
         numberOfOperations++;
       }
     }
+    result[pivotPoint]=closestNeighbor;
   }
   return numberOfOperations;
 }
@@ -138,14 +151,14 @@ Basically it looks for the closest neighbor in all the matrix for some points.
 While doing so it classfies the other points by distance, creating regions with different radius around that pivot point.
 Then checks all the points within that region cause probability says that the nearest neighbor of that point should be within the same region.
 */
-long personalClosestNeighbor(Point *matrix,int matrixLength){
+long personalClosestNeighbor(Point *matrix,int matrixLength,int *result){
   double borderRadius = calculateAbsLongestDistance()*4/10;
-  double targetRadius = calculateAbsLongestDistance()*3/10;
+  double targetRadius = calculateAbsLongestDistance()/5;
   char visited[NUMBER_OF_POINTS];
   int numberOfPointsVisited = 0;
   int pivotPoint=0;
   while(numberOfPointsVisited<NUMBER_OF_POINTS && pivotPoint>-1){
-    findingNearestNeighbor(&matrix[0],matrixLength,pivotPoint,&numberOfPointsVisited,borderRadius,targetRadius,&visited[0]);
+    findingNearestNeighbor(&matrix[0],matrixLength,pivotPoint,&numberOfPointsVisited,borderRadius,targetRadius,&visited[0],result);
     pivotPoint=findingNextPivotPoint(&visited[0],matrixLength);
   }
   return 0;
@@ -155,7 +168,7 @@ long personalClosestNeighbor(Point *matrix,int matrixLength){
 The actual function that divides into regions and searches,
 I decided to make it this way so recursion is easier to handle.
 */
-int findingNearestNeighbor(Point *matrix,int matrixLength,int pivotPoint,int *numberOfPointsVisited,double borderRadius,double targetRadius,char *visitedPointsArray){
+int findingNearestNeighbor(Point *matrix,int matrixLength,int pivotPoint,int *numberOfPointsVisited,double borderRadius,double targetRadius,char *visitedPointsArray,int *result){
   int numberOfClosePoints=0,numberOfNotSoClosePoints=0;
   long double distance=ABSURD_DISTANCE,newDistance;
   int closestPointIndex = -1;
@@ -187,25 +200,27 @@ int findingNearestNeighbor(Point *matrix,int matrixLength,int pivotPoint,int *nu
       }
     }
   }
-
+  result[pivotPoint]=closestPointIndex;
   visitedPointsArray[pivotPoint]++;
   /**
   *Now that the point of the near vecinity have been identified we can proceed with the checking of said points.
   */
   //This "if" is used in the not so probable case that all of the points are closed together, causing the algorithm to go O(n^2), again. Thus slowing it down.
-  if(numberOfClosePoints+numberOfNotSoClosePoints>=matrixLength){
-    double newBorderRadius = borderRadius*4/10;
-    double newTargetRadius = targetRadius*3/10;
-    findingNearestNeighbor(matrix,matrixLength,pivotPoint,numberOfPointsVisited,newBorderRadius,newTargetRadius,visitedPointsArray);
-  }
+  // if(numberOfClosePoints+numberOfNotSoClosePoints>=matrixLength){
+  //   double newBorderRadius = borderRadius*4/10;
+  //   double newTargetRadius = targetRadius*3/10;
+  //   findingNearestNeighbor(matrix,matrixLength,pivotPoint,numberOfPointsVisited,newBorderRadius,newTargetRadius,visitedPointsArray,result);
+  // }
   //From here on what we will be checking are the point of the near vecinity of the original pivotPoint
-  else{
+  //else{
     Point pivot;
     //Starts in one cause the first element was the original pivot and we know we already found its closest Neigbor
     for(int i=1;i<numberOfClosePoints;i++){
       distance=ABSURD_DISTANCE;
-      if(visitedPointsArray[closePointsIndexes[i]]<1){ //If that point is still not checked
-        pivot=matrix[closePointsIndexes[i]];
+      closestPointIndex=-1;
+      pivotPoint=closePointsIndexes[i];
+      if(visitedPointsArray[pivotPoint]<1){ //If that point is still not checked
+        pivot=matrix[pivotPoint];
         //First we check the points that are possibly closest to the pivot.
         for(int j=0;j<numberOfClosePoints;j++){
           if(j!=i){ //they are not the same point
@@ -224,21 +239,30 @@ int findingNearestNeighbor(Point *matrix,int matrixLength,int pivotPoint,int *nu
             distance=newDistance;
           }
         }
-        if(closestPointIndex>-1){//If a neighbor was found in that region. (weird scenario but could happen)
+        if(closestPointIndex>-1){//If a neighbor was not found in that region. (weird scenario but could happen)
           visitedPointsArray[closePointsIndexes[i]]++;//Mark that point as visited
+          result[pivotPoint]=closestPointIndex;//Saves the index of the closest neighbor in the results array
+          //printf("%d\n", closestPointIndex);
         }
       }
     }
-  }
+  //}
   return 0;
 }
 
-/**
-*Converts a structure Point into a String pretty much like method
-*toString() in Java.
-*/
-// char* PointToString(Point p){
-//   char* string = "(";
-//   string+=itoa(p.x)+','+itoa(p.y);
-//   return string+')';
-// }
+//Testes if the answer of both algoritms is the same and therefore correct.
+int test(int *answer1,int *answer2,int length){
+  char q=0;
+  for(int i=0;i<length;i++){
+    if(answer1[i]!=answer2[i]){
+      printf("%d,%d\n",answer1[i],answer2[i]);
+      if(q==0){
+        q++;
+      }
+    }
+  }
+  if(q){
+    return 0;
+  }
+  return 1;//True
+}
