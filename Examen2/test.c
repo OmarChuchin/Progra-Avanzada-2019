cd8412862c612a6db7741e80b54a6ec8a1b77c78
Omar Cuenca <A01378844@itesm.mx>
2019-10-06 20:30:44 -0500

    Exam 2 Commit 2

---
+++ Examen2/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+enum MinutiaType = {Ending,Bifurcation,Unknown};
+
+typedef struct minut
+{
+    unsigned short x,y;
+    float angle;
+
+
+} Minutia;
+
+
+int main(int argc, char const *argv[]){
+    /* code */
+    return 0;
+}
